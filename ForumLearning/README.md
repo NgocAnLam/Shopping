d396aff1fb32db5e652dbeb73cef997eef25f885
Ngọc Ẩn <62063562+NgocAnLam@users.noreply.github.com>
2024-01-07 15:37:07 +0700

    Delete ForumLearning directory

--- ForumLearning/README.md
+++
@@ -1,3 +0,0 @@
-# forum-learning-android
-
-Đồ án môn học Phát triển ứng dụng trên thiết bị di động - NT118.M12.MMCL
--- ForumLearning/app/src/main/java/com/example/forumlearning/ui/signup/SignupViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.forumlearning.ui.signup;
-
-import androidx.lifecycle.ViewModel;
-
-public class SignupViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
